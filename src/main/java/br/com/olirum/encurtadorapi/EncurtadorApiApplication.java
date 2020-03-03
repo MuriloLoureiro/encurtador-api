e3102ec21bc0d475c8fb3e0bd8973568865b0930
MuriloLoureiro <muriloh.loureiro@gmail.com>
2020-03-03 01:57:12 -0300

    Encurtador de Urls Starter]

---
+++ src/main/java/br/com/olirum/encurtadorapi/EncurtadorApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.olirum.encurtadorapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncurtadorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncurtadorApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/olirum/encurtadorapi/EncurtadorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.olirum.encurtadorapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncurtadorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
